455d15b8c83dc14607da09f6738610e1da856d15
Montoev <148058918+Montoev@users.noreply.github.com>
2023-12-12 23:55:52 +0300

    Add files via upload

---
+++ js1/main.js
@@ -0,0 +1,10 @@
+let firstName = 'Aleksander'
+let age = '37'
+
+// if(age > 18) {
+//   console.log('Вы совершеннолетний')
+// } else{
+//   console.log('Вам нет 18')
+// }
+
+age >= 18 ? console.log('Вы совершеннолетниий') : console.log('Вам нет 18')
---
+++ js81/main.js
@@ -0,0 +1,16 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// for (let i = 1; i < numbers.length; i = i + 2) {
+//   console.log(numbers[i])
+// }
+
+// const color = ['красный', 'оранжевый', 'желтый', 'зеленый', 'голубой', 'синий', 'фиолетовый']
+
+// for (let i = 0; i < color.length; i++) {
+//   console.log(color[i])
+// }
+
+const color = ['красный', 'оранжевый', 'желтый', 'зеленый', 'голубой', 'синий', 'фиолетовый']
+
+for (let i = 6; i >= 0; i = i - 1) {
+  console.log(color[i])
+}
