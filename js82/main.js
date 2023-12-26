a1b0953924d18de8ea6f5c4b4455b08b2787a8ba
Montoev <148058918+Montoev@users.noreply.github.com>
2023-12-26 12:09:46 +0300

    Add files via upload

--- js82/main.js
+++ js82/main.js
@@ -10,34 +10,33 @@
 // }
 // console.log(chekcForCopyItem(users, 'Alex'))
 
-// const numbers = [2, 4, 7, 9, 10, 14, 17]
-// function chekcForCopyItem (array, item) {
-//   for (let i = 0; i < array.length; i++) {
-//     if (array [i] && item > 10) {
-//       return `${item}`
-//     }
+const numbers = [2, 4, 7, 9, 10, 14, 17]
+const numbers2 = []
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 10) {
+    numbers2.push(numbers[i])
+    }
+  }
+
+console.log(numbers2)
+
+
+// function calculator(firstNumber, secondNumber, operator) {
+//   if (operator === '+') {
+//     return firstNumber + secondNumber
 //   }
-//   return false
+//   if (operator === '-') {
+//     return firstNumber -   secondNumber
+//   }
+//   if (operator === '*') {
+//     return firstNumber * secondNumber
+//   }
+//   if (operator === '/' && secondNumber != 0) {
+//     return firstNumber / secondNumber
+//   }
+//   else {
+//     return 'false'
+//   }
 // }
-// console.log(chekcForCopyItem(numbers, 17))
 
-
-function calculator(firstNumber, secondNumber, operator) {
-  if (operator === '+') {
-    return firstNumber + secondNumber
-  }
-  if (operator === '-') {
-    return firstNumber -   secondNumber
-  }
-  if (operator === '*') {
-    return firstNumber * secondNumber
-  }
-  if (operator === '/' && secondNumber != 0) {
-    return firstNumber / secondNumber
-  }
-  else {
-    return 'false'
-  }
-}
-
-console.log(calculator(4, 0, '/'))
+// console.log(calculator(4, 0, '/'))
